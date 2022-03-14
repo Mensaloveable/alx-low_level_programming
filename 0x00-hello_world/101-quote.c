572d75b609b28d4175d625aa6be2b176fcb7507e
Mensaloveable <abdullahi.adedayo@gmail.com>
2022-03-13 23:37:13 -0700

    hope

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *My hello, printf task
+ *Return: return 1 after progress execution
+ */
+int main(void)
+{
+putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
