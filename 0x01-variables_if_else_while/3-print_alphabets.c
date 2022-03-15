71c4722e0d3138742b7e5fa17df4ee7303d6d147
Mensaloveable <abdullahi.adedayo@gmail.com>
2022-03-14 20:12:50 -0700

    3rd

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,11 @@ int main(void)
 {
 char upper_alpha;
 char lower_alpha;
-for (lower_alpha = 'a'; alphabet <= 'z'; lower_alpha++)
+for (lower_alpha = 'a'; lower_alpha <= 'z'; lower_alpha++)
 {
 putchar(lower_alpha);
 }
-for (upper_alpha = 'A'; alphabet <= 'Z'; upper_alpha++)
+for (upper_alpha = 'A'; upper_alpha <= 'Z'; upper_alpha++)
 {
 putchar(upper_alpha);
 }
