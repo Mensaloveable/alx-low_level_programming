7c8ba7b4d07c226210bb0427877353d671bb4555
Mensaloveable <abdullahi.adedayo@gmail.com>
2022-03-14 20:10:39 -0700

    3rd

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+char upper_alpha;
+char lower_alpha;
+for (lower_alpha = 'a'; alphabet <= 'z'; lower_alpha++)
+{
+putchar(lower_alpha);
+}
+for (upper_alpha = 'A'; alphabet <= 'Z'; upper_alpha++)
+{
+putchar(upper_alpha);
+}
+putchar('\n');
+return (0);
+}
+
